ce2651817814ee34e3b0fc2f208be0483dd55de0
haniputraa0607 <hanifmurtadha0607@gmail.com>
2021-08-16 18:52:04 +0700

    hapus duplikat dan dapat angka 2 terbesar soal1

--- soal1.php
+++ soal1.php
@@ -1,8 +1,15 @@
 <?php 
   // SOAL 1
   echo "SOAL 1 <br><br>";
-  $array = [6,3,5,4,2,3];
-  cariduaterbesar($array);
+  $array = [6,3,5,4,2,3,4];
+  // var_dump($array);
+  // echo "<br>";
+  for($i=0;$i<=count($array)-1;$i++){
+    $urut = $i+1;
+    echo "Angka ke $urut adalah $array[$i] <br>";
+  }
+  $duaterbesar = cariduaterbesar($array);
+  echo "Angka ke-2 terbesar dari array adalah $duaterbesar";
   function cariduaterbesar($array){
     //mengurutkan array terkecil-terbesal
     $panjang = count($array)-1;
@@ -15,7 +22,21 @@
         }
       }
     }
-
+    // var_dump($array);
+    echo "<br>";
+    $index=0;
+    for($i=0;$i<=$panjang;$i++){
+      $awal = $array[$i];
+      $akhir = $array[$i+1];
+      if($awal!=$akhir){
+        $tanpaduplikat[$index]=$awal;
+        $index++;
+      }    
+    }
+    // var_dump($tanpaduplikat);
+    // echo "<br>";
+    // echo $tanpaduplikat[count($tanpaduplikat)-2];
+    return $tanpaduplikat[count($tanpaduplikat)-2];
   }
 
 
